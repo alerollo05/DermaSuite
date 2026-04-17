7c7c41b8a5c8cc06bb875a6855dd1f079120f540
feffo32 <fedemarialux@gmail.com>
2026-04-17 09:43:01 +0200

    commit librerie

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,12 @@ plugins {
 
 android {
     namespace = "it.uninsubria.dermasuite"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "it.uninsubria.dermasuite"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
