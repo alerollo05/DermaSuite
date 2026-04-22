d1e6448556fbb83d2a8a3b609c80ac479bb01a2f
feffo32 <fedemarialux@gmail.com>
2026-04-22 21:22:37 +0200

    commit dashboard ciao nome ute

--- app/src/main/java/it/uninsubria/dermasuite/ui/components/DermaBottomBar.kt
+++ app/src/main/java/it/uninsubria/dermasuite/ui/components/DermaBottomBar.kt
@@ -1,12 +1,13 @@
 package it.uninsubria.dermasuite.ui.components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.graphics.BlendMode.Companion.Screen
+import androidx.navigation.NavController
+import androidx.navigation.compose.currentBackStackEntryAsState
 
 @Composable
-fun DermaBottomBar(
+fun DermaBottomBar(){
 
-
-
-){
 
 }
